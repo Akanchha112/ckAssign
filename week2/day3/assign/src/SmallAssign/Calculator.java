15357270362892ea48251f9dca9a0a135a8394a3
Akanchha112 <sakanchha111@gmail.com>
2025-11-10 00:20:37 +0530

    day3 and day4 assign

---
+++ week2/day3/assign/src/SmallAssign/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    int first;
+    int second;
+    void setter(int f,int s){
+        this.first=f;
+        this.second=s;
+    }
+    int add(){
+        return this.first+this.second;
+    }
+    int subtract(){
+        return this.first-this.second;
+    }
+    int multiply(){
+        return this.first*this.second;
+    }
+    double divide(){
+        return this.first/this.second;
+    }
+
+}
